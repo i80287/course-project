2272d13af7f4f1f53af2065b3302beb05d4fe25f
i80287 <addmlbx@gmail.com>
2024-03-30 18:05:34 +0300

    update scripts according to folders renaming

--- run_clang_format.sh
+++ run_clang_format.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-clang-format -i ./*.cpp ./App/*.cpp ./GraphicsUtils/*.cpp -style=file:./.clang-format -fallback-style=Google
+clang-format -i ./*.cpp ./App/*.cpp ./App/*.hpp ./GraphicsUtils/*.cpp ./GraphicsUtils/*.hpp -style=file:./.clang-format -fallback-style=Google
