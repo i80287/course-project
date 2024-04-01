8fd36df58ee88e35dc2b01351df2f756d36a25b9
i80287 <addmlbx@gmail.com>
2024-04-02 01:12:20 +0300

    updated logger

--- GraphicsUtils/DrawerUtils/Logger.hpp
+++ GraphicsUtils/DrawerUtils/Logger.hpp
@@ -12,10 +12,25 @@ public:
                       std::source_location::current()) const
         noexcept(!kIsDebugLogEnabled) {
         if constexpr (kIsDebugLogEnabled) {
-            std::clog << "[DEBUG] [Message: '" << msg
+            std::clog << "[DEBUG] [message: '" << msg
                       << "'] [source: " << location.file_name() << ", line "
                       << location.line() << ':' << location.column() << "] ["
                       << location.function_name() << ']' << std::endl;
+        }
+    }
+
+    void DebugLog(std::string_view str1, std::string_view str2, char sep = '\0',
+                  const std::source_location location =
+                      std::source_location::current()) const
+        noexcept(!kIsDebugLogEnabled) {
+        if constexpr (kIsDebugLogEnabled) {
+            std::string s(str1.size() + str2.size(), '\0');
+            s += str1;
+            if (sep != '\0') {
+                s.push_back(sep);
+            }
+            s += str2;
+            DebugLog(s, location);
         }
     }
 
